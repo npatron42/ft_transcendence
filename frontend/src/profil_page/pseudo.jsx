fe30f69fbda7918b1be9fcdeb7204db77568e8fa
Ilyes Souaidia <leilyesdu06300@gmail.com>
2024-10-30 20:23:09 +0100

    change user ok

--- frontend/src/profil_page/pseudo.jsx
+++ frontend/src/profil_page/pseudo.jsx
@@ -3,63 +3,64 @@ import { Form, Button } from 'react-bootstrap';
 import { useAuth } from '../provider/UserAuthProvider';
 import { useTranslation } from 'react-i18next';
 import { getUser, sendName } from '../api/api';
-import './pseudo.css'
+import './pseudo.css';
 
-function Pseudo({Actif, setActif}) {
-
+function Pseudo({ Actif, setActif }) {
 	const { t } = useTranslation();
-
-	const {myUser, setUser} = useAuth();
+	const { myUser, setUser } = useAuth();
 	const [input, setInput] = useState(myUser.username || '');
 	const [modif, setModif] = useState(false);
 	const [valide, setValide] = useState(false);
 	const [errorMessage, setErrorMessage] = useState("");
 
 	const handleClick = async () => {
-		
 		if (modif) {
 			const usernameRegex = /^[a-zA-Z0-9.-]{3,11}$/;
-			
+
 			if (input === '') {
 				setErrorMessage("registerPage.idRequired");
 				return;
-			}
-	
-			else if (!usernameRegex.test(input)) {
+			} else if (!usernameRegex.test(input)) {
 				setErrorMessage("registerPage.idCara");
 				return;
 			}
-			try
-			{
+
+			try {
 				const response = await sendName(input);
-				if (response.success)
-				{
+				if (response.success) {
 					const tmpUser = await getUser();
 					setUser(tmpUser);
-					setInput(''); 
+					setInput('');
 					setModif(false);
 					setActif(false);
 					setValide(false);
 					setErrorMessage('');
-				}
-				else if (response.success === false)
+				} else if (response.success === false) {
 					setErrorMessage("profilPage.errorUser");
-				else
+				} else {
 					console.log("marche pas");
-				} catch (error) {
-					alert('Failed');
-				  }
-			} else {
+				}
+			} catch (error) {
+				console.log('Failed user');
+			}
+		} else {
 			setInput('');
 			setValide(true);
 			setActif(true);
 			setModif(true);
 		}
-	}
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (modif) {
+			handleClick();
+		}
+	};
 
 	return (
 		<div>
-			<Form>
+			<Form onSubmit={handleSubmit}>
 				<p className="para-ps">{t("registerPage.id")}</p>
 				<Form.Group className="input-ps" controlId="User">
 					<Form.Control
@@ -68,15 +69,15 @@ function Pseudo({Actif, setActif}) {
 						value={modif ? input : myUser.username}
 						onChange={(e) => setInput(e.target.value)}
 						readOnly={!modif}
-						className="form-test" 
+						className="form-test"
 					/>
 				</Form.Group>
 			</Form>
-			
+
 			{errorMessage && <p className="error-pseudo">{t(errorMessage)}</p>}
-			
-			<Button variant="outline-dark" className="custom-pseudo" 
-			onClick={handleClick}  disabled={Actif && !valide}>
+
+			<Button variant="outline-dark" className="custom-pseudo"
+				onClick={handleClick} disabled={Actif && !valide}>
 				{modif ? t("profilPage.valide") : t('profilPage.modif')}
 			</Button>
 		</div>
