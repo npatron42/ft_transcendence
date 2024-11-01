2cc24e46258682c29129fc48a1e88eb09c72d888
newpc <leilyesdu06300@gmail.com>
2024-11-01 16:16:31 +0100

    add front Rgpd

---
+++ frontend/src/profil_page/recProfil.jsx
@@ -0,0 +1,16 @@
+import { Button } from 'react-bootstrap';
+import './utils.css';
+
+function recProfil() {
+  return (
+	<div>	
+		<Button 
+		variant="outiline-dark" 
+		className="custom-rgpd">
+			Recuperer mes donnes
+		</Button>
+	</div>
+  )
+}
+
+export default recProfil
