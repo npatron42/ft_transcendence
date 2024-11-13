8d0aa894bd26052fd4847527e181ba3972144912
Fpalumbo42 <fabio.palumbo@hotmail.fr>
2024-11-13 03:55:11 +0100

    le poulet

--- frontend/src/game_page/settings/GameSettings.jsx
+++ frontend/src/game_page/settings/GameSettings.jsx
@@ -3,12 +3,69 @@ import '../css/gameSettings.css';
 import { useWebSocket } from '../../provider/WebSocketProvider';
 import { useAuth } from '../../provider/UserAuthProvider';
 import { getGameSettings, updateGameSettings } from '../../api/api';
+import MiniPong from './MiniPong';
+
+const Carousel = ({ items, currentItem, onSelectItem, type }) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    if (!items) {
+        return <div>Loading...</div>;
+    }
+
+    const handleNext = () => {
+        setCurrentIndex((prevIndex) => {
+            const nextIndex = (prevIndex + 1) % items.length;
+            onSelectItem(items[nextIndex].name, type);
+            return nextIndex;
+        });
+    };
+
+    const handlePrev = () => {
+        setCurrentIndex((prevIndex) => {
+            const prevIndexTmp = (prevIndex - 1 + items.length) % items.length;
+            onSelectItem(items[prevIndexTmp].name, type);
+            return prevIndexTmp;
+        });
+    };
+
+    return (
+        <div className="carouselContainer">
+            <button onClick={handlePrev} className="carouselButton">Prev</button>
+            <div className="carouselContent">
+                <div
+                    className={`carouselItem ${items[currentIndex].className}`}
+                    style={items[currentIndex].style}
+                ></div>
+                <div className="itemName">{items[currentIndex].name}</div>
+            </div>
+            <button onClick={handleNext} className="carouselButton">Next</button>
+        </div>
+    );
+};
+
 
 const GameSettings = () => {
-    const { websocketUser } = useWebSocket();
     const { myUser } = useAuth();
     const [gameSettings, setGameSettings] = useState();
     const [keyBind, setKeyBind] = useState({ up: "w", down: "s" });
+
+    const paddleSkin = [
+        { name: 'Classic', className: 'classicSkin', style: { backgroundColor: '#ffffff' }},
+        { name: 'Neon', className: 'neonSkin', style: { backgroundColor: '#00FF00' }},
+        { name: 'Fire', className: 'fireSkin', style: { backgroundColor: '#FF4500' }},
+    ];
+
+    const ballSkin = [
+        { name: 'Blue', className: 'blueBall', style: { backgroundColor: '#0000FF' } },
+        { name: 'Red', className: 'redBall', style: { backgroundColor: '#FF0000' } },
+        { name: 'Green', className: 'greenBall', style: { backgroundColor: '#00FF00' } },
+    ];
+
+    const boardSkin = [
+        { name: 'Black', className: 'blackBoard', style: { backgroundColor: '#000000' } },
+        { name: 'Gray', className: 'grayBoard', style: { backgroundColor: '#808080' } },
+        { name: 'White', className: 'whiteBoard', style: { backgroundColor: '#FFFFFF' } },
+    ];
 
     useEffect(() => {
         (async () => {
@@ -28,6 +85,8 @@ const GameSettings = () => {
                 up: keyBind.up,
                 down: keyBind.down,
                 paddleSkin: gameSettings.paddleSkin,
+                ballColor: gameSettings.ballColor,
+                boardColor: gameSettings.boardColor,
             };
             await updateGameSettings(updatedSettings);
             console.log("updatedSettings", updatedSettings);
@@ -46,6 +105,10 @@ const GameSettings = () => {
         });
     };
 
+    const handleSelectItem = (itemName, type) => {
+        setGameSettings({ ...gameSettings, [type]: itemName });
+    };
+
     if (!gameSettings) {
         return <div>Loading...</div>;
     }
@@ -53,48 +116,77 @@ const GameSettings = () => {
     return (
         <div className="gameSettingsGlobalContainer">
             <div className="gameSettingsContainer">
-                <h1>Game Settings</h1>
                 <div className="gameSettings">
-                    <div className="keyBindContainer">
-                        <h2>Key Bind</h2>
-
-
-                        <section class="container">
-                            <label>
-                                <input type="checkbox" name="check" onClick={() => handleKeyBindClick("up")} />
-                                <span></span>
-                                <p>{keyBind.up}</p>
-                            </label>
-                        </section>
-                        <section class="container">
-                            <label>
-                                <input type="checkbox" name="check"   onClick={() => handleKeyBindClick("down")} />
-                                <span></span>
-                                <p>{keyBind.down}</p>
-                            </label>
-                        </section>
+                    <div className="leftContainer">
+                        <div className="miniPongContainer">
+                            <MiniPong />
                         </div>
-
-
-                        <div className="paddleSkinContainer">
-                            <h2>Paddle Skin</h2>
-                            <input
-                                type="text"
-                                placeholder="Paddle Skin"
-                                value={gameSettings.paddleSkin}
-                                onChange={(e) => setGameSettings({ ...gameSettings, paddleSkin: e.target.value })}
-                            />
+                        <div className="keyBindContainer">
+                            <div className="sectionHeader">
+                                <h2>Key Bind</h2>
+                            </div>
+                            <section className="container">
+                                <label>
+                                    <input type="checkbox" name="check" onClick={() => handleKeyBindClick("up")} />
+                                    <span> <i className="bi bi-caret-up-square"></i> {keyBind.up}</span>
+                                </label>
+                            </section>
+                            <section className="container">
+                                <label>
+                                    <input type="checkbox" name="check" onClick={() => handleKeyBindClick("down")} />
+                                    <span> <i className="bi bi-caret-down-square"></i> {keyBind.down}</span>
+                                </label>
+                            </section>
                         </div>
                     </div>
-                    <button
-                        className="buttonSaveSettings"
-                        onClick={handleSaveSettings}
-                    >
-                        Save Settings
-                    </button>
+                    <div className="SkinContainer">
+                        <div className="sectionHeader">
+                            <h2>Skin</h2>
+                        </div>
+                    <div className="PaddleSkinContainer">
+                        <Carousel
+                            items={paddleSkin}
+                            currentSkin={gameSettings.paddleSkin}
+                            onSelectItem={handleSelectItem}
+                            type="paddleSkin"
+                        />
+                    </div>
+
+                    <div className="BallSkinContainer">
+                        <div className="sectionHeader">
+                            <h2>Ball Color</h2>
+                        </div>
+                        <Carousel
+                            items={ballSkin}
+                            currentItem={gameSettings.ballSkin}
+                            onSelectItem={handleSelectItem}
+                            type="ballSkin"
+                        />
+                    </div>
+
+                    <div className="BoardSkinContainer">
+                        <div className="sectionHeader">
+                            <h2>Board Color</h2>
+                        </div>
+                        <Carousel
+                            items={ boardSkin}
+                            currentItem={gameSettings.boardSkin}
+                            onSelectItem={handleSelectItem}
+                            type="boardSkin"
+                        />
+                    </div>
+                    </div>
+
                 </div>
+                <button
+                    className="buttonSaveSettings"
+                    onClick={handleSaveSettings}
+                >
+                    Save Settings
+                </button>
             </div>
-            );
+        </div>
+    );
 };
 
-            export default GameSettings;
+export default GameSettings;
